9b87830cb56d2ef9ab70b0fef20cd51931f2f54f
andrii-m142-1 <hovorukha.a.y@nmu.one>
2022-12-08 16:18:28 +0200

    lab5

--- src/main/java/com/lab5/controller/MainController.java
+++ src/main/java/com/lab5/controller/MainController.java
@@ -13,11 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MainController {
     @RequestMapping("/index")
-    String index(){
+    public String index(){
         return "index";
     }
     @RequestMapping("/home")
-    String home(@RequestParam String name,Model model){
+    public String home(@RequestParam String name,Model model){
         model.addAttribute("guest_name",name);
         return "home";
     }
